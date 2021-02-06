5a5638703822ce4f7a8a5e1409471559ff7e54f5
Zoe Bingham <zmb6893@g.rit.edu>
2021-02-06 05:47:44 -0800

    Workspace initialized

---
+++ UpGrowth/UpGrowth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpGrowth
+//
+//  Created by Zoe Bingham on 2/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
